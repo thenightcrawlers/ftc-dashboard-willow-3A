5d4abc6f3c8a0f7d98698edd6b14484488514201
thenightcrawlers <conor_moore.stu@willowschoolnola.org>
2023-09-01 11:10:26 -0500

    Update HelloWorldConor.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldConor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldConor.java
@@ -3,7 +3,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp()
-public class HelloWorld extends OpMode {
+public class HelloWorldConor extends OpMode {
     @Override
     public void init() {
         telemetry.addData("Hello", "World");
