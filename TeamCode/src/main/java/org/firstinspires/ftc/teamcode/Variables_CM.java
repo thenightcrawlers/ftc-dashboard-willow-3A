2aab6614b277da48581898b5d16c5c2edf0a348c
thenightcrawlers <conor_moore.stu@willowschoolnola.org>
2023-09-05 10:59:52 -0500

    Create Variables_CM.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_CM.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class Variables_CM extends OpMode {
+    @Override
+    public void init() {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        int teamNumber = 16072;
+        double motorSpeed = 0.5;
+        boolean touchSensorPressed = true;
+
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("Motor Speed", motorSpeed);
+        telemetry.addData("Touch Sensor", touchSensorPressed);
+    }
+    @Override
+    public void loop() {
+
+    }
+}
