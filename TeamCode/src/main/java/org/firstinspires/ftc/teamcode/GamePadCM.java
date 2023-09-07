eac6520a29343b19cd2bac48e86e731837ccb1e0
thenightcrawlers <conor_moore.stu@willowschoolnola.org>
2023-09-07 11:23:44 -0500

    Update GamePadCM.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadCM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadCM.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 
 @TeleOp()
 public class GamePadCM extends OpMode {
@@ -13,5 +15,14 @@ public class GamePadCM extends OpMode {
         telemetry.addData("Left stick x", gamepad1.left_stick_x);
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
         telemetry.addData("A button", gamepad1.a);
+        if(gamepad1.left_stick_y < 0) {
+        telemetry.addData("Left stick y", "is negative");
+    }
+        telemetry.addData("Left Stick y", gamepad1.left_stick_y);
+        if(gamepad1.left_stick_x < 0) {
+            telemetry.addData("Left stick x", "is negative");
+        }
+        telemetry.addData("Left Stick x", gamepad1.left_stick_x);
+        telemetry.update();
     }
 }
