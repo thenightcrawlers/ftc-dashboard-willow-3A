4eb77c87f50fffd8c5f9d7e4373f5824bf430c33
thenightcrawlers <conor_moore.stu@willowschoolnola.org>
2023-09-11 11:26:43 -0500

    Update GamePadCM.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadCM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadCM.java
@@ -14,15 +14,20 @@ public class GamePadCM extends OpMode {
     public void loop() {
         telemetry.addData("Left stick x", gamepad1.left_stick_x);
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        telemetry.addData("Right stick x", gamepad1.right_stick_x);
+        telemetry.addData("Right stick y", gamepad1.right_stick_y);
+        telemetry.addData("Left Trigger",gamepad1.left_trigger);
+        telemetry.addData("Right Trigger", gamepad1.right_trigger);
+        telemetry.addData("Left Trigger + Right Trigger", gamepad1.left_trigger - gamepad1.right_trigger);
         telemetry.addData("A button", gamepad1.a);
+        telemetry.addData("B button", gamepad1.b);
+        telemetry.addData("Difference between Left Joystick Y and Right Joystick Y", gamepad1.left_stick_y - gamepad1.right_stick_y);
         if(gamepad1.left_stick_y < 0) {
         telemetry.addData("Left stick y", "is negative");
     }
-        telemetry.addData("Left Stick y", gamepad1.left_stick_y);
         if(gamepad1.left_stick_x < 0) {
             telemetry.addData("Left stick x", "is negative");
         }
-        telemetry.addData("Left Stick x", gamepad1.left_stick_x);
         telemetry.update();
     }
 }
